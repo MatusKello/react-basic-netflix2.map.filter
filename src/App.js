7835476a2c3fe80f48aa06bb04c5ab5bac0151f3
Matus Kello <m.kello1992@gmail.com>
2023-06-25 20:27:42 +0200

    added back button and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import allMovies from './data';
 import categories from './categories';
 
 const App = () => {
   const [typeOfMovie, setTypeOfMovie] = useState('komedie');
+  const [mainPage, setMainPage] = useState(true);
 
   const vysledneFilmy = allMovies.filter((oneMovie) => {
     return oneMovie['category'] === typeOfMovie;
@@ -11,34 +12,48 @@ const App = () => {
 
   return (
     <div>
-      <div className='all-buttons'>
-        {categories.map((oneCategory, index) => {
-          return (
-            <button
-              className='one-button'
-              key={index}
-              onClick={() => setTypeOfMovie(oneCategory)}
-            >
-              {oneCategory}
-            </button>
-          );
-        })}
-      </div>
-      <div className='all-movies'>
-        {vysledneFilmy.map((oneMovie) => {
-          const { id, image, title, age, tags, description } = oneMovie;
+      {mainPage ? (
+        <div className=''>
+          <h1 className='one-movie'>Netflix</h1>
+          <button className='one-button' onClick={() => setMainPage(false)}>
+            Categories
+          </button>
+        </div>
+      ) : (
+        <div>
+          <div className='all-buttons'>
+            {categories.map((oneCategory, index) => {
+              return (
+                <button
+                  className='one-button'
+                  key={index}
+                  onClick={() => setTypeOfMovie(oneCategory)}
+                >
+                  {oneCategory}
+                </button>
+              );
+            })}
+          </div>
+          <div className='all-movies'>
+            {vysledneFilmy.map((oneMovie) => {
+              const { id, image, title, age, tags, description } = oneMovie;
 
-          return (
-            <div className='one-movie' key={id}>
-              <img src={image} alt='' />
-              <h2>{title}</h2>
-              <p>{age}</p>
-              <p>{tags}</p>
-              <p>{description}</p>
-            </div>
-          );
-        })}
-      </div>
+              return (
+                <div className='one-movie' key={id}>
+                  <img src={image} alt='' />
+                  <h2>{title}</h2>
+                  <p>{age}</p>
+                  <p>{tags}</p>
+                  <p>{description}</p>
+                </div>
+              );
+            })}
+          </div>
+          <button className='back-button' onClick={() => setMainPage(true)}>
+            Back
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/categories.jsx
+++ src/categories.jsx
@@ -1,3 +1,62 @@
 const categories = ['komedie', 'romantický', 'akční', 'horor'];
 
 export default categories;
+
+/* import { useState } from 'react';
+import allMovies from './data';
+import categories from './categories';
+
+const App = () => {
+  const [typeOfMovie, setTypeOfMovie] = useState('komedie');
+  const [mainPage, setMainPage] = useState(true);
+
+  const MainPage = ({ setMainPage }) => {
+    return (
+      <div>
+        <h1>Netflix</h1>
+        <button onClick={() => setMainPage(false)}>Categories</button>
+      </div>
+    );
+  };
+
+  const vysledneFilmy = allMovies.filter((oneMovie) => {
+    return oneMovie['category'] === typeOfMovie;
+  });
+
+  return (
+    <div>
+      {mainPage} ?(
+      <MainPage setMainPage={setMainPage} />)
+      <div className='all-buttons'>
+        {categories.map((oneCategory, index) => {
+          return (
+            <button
+              className='one-button'
+              key={index}
+              onClick={() => setTypeOfMovie(oneCategory)}
+            >
+              {oneCategory}
+            </button>
+          );
+        })}
+      </div>
+      <div className='all-movies'>
+        {vysledneFilmy.map((oneMovie) => {
+          const { id, image, title, age, tags, description } = oneMovie;
+
+          return (
+            <div className='one-movie' key={id}>
+              <img src={image} alt='' />
+              <h2>{title}</h2>
+              <p>{age}</p>
+              <p>{tags}</p>
+              <p>{description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App; */
